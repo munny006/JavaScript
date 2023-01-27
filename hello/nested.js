174afd9c149ba58ee33bda24f9780ec217a52274
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-01-27 15:48:34 +0600

    nested

---
+++ hello/nested.js
@@ -0,0 +1,20 @@
+var taka = 400;
+var bread = 24;
+var read = 14;
+var ead = 343;
+var ad = 44;
+if(bread < taka){
+    console.log('hello bread');
+}
+else if(read < taka){
+    console.log('hello read');
+}
+else if(ead < taka){
+    console.log('hello ead');
+}
+else if(ad < taka){
+    console.log('hello ad');
+}
+else{
+    console.log('Tumr Kicchu kora lagbe na');
+}
