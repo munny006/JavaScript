76c6ba308fd865f61a3bbcbbd43821c38b8de7c5
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-01-27 15:36:27 +0600

    array

---
+++ hello/array.js
@@ -0,0 +1,12 @@
+var frinds = ['salma','halma','maisha'];
+console.log(frinds);
+
+console.log(frinds.length);
+
+//index
+
+console.log(frinds[0]);
+
+//position
+var positionIndex =  frinds.indexOf(78);
+console.log(positionIndex);
