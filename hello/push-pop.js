f9756d802e268cef283cb200ed2aa203ad0a3149
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-01-27 15:41:53 +0600

    push-pop

---
+++ hello/push-pop.js
@@ -0,0 +1,15 @@
+var num1 = [23,27,99,85,98];
+console.log(num1);
+
+//push
+num1.push(100);
+console.log(num1);
+
+//pop
+num1.pop()
+console.log(num1);
+
+//return
+num1.pop();
+var element = num1.pop();
+console.log(element);
