d57f3ace10e2b93ebccb32309d1335659e72189d
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-02-03 00:16:38 +0600

    remove array slice/splic

---
+++ explore/remove.js
@@ -0,0 +1,9 @@
+const friends = [12,33,54,66,78,63,88,675,22,14];
+
+
+//slice
+const partial = friends.slice(2,5);
+console.log(partial);
+
+const proper = friends.splice(4,6)
+console.log(proper);
