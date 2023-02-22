5dd2f29ae84d223e257cc170e42678ef60eeb305
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-02-22 20:52:13 +0600

    es6 conceptual

---
+++ Es6-conceptual/letConstant.js
@@ -0,0 +1,12 @@
+//var using
+var name = 'Mahmuda';
+name = 'akter'
+//let using
+let Name1 = 'maisha'
+Name1 = 'islam'
+//const
+const Name2 = 'munny';
+
+console.log(name);
+console.log(Name1);
+console.log(Name2);
