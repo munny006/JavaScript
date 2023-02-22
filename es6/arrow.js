58c3cce5f0a0134de48595b041054a3e118d8f73
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-02-22 11:43:50 +0600

    es6 more arrow.js

---
+++ es6/arrow.js
@@ -0,0 +1,6 @@
+function add (first,second){
+    const total = first + second;
+    return total;
+}
+const result = add(10,20);
+console.log(result);
---
+++ es6/morearrow.js
@@ -0,0 +1,5 @@
+const add = (first,second) =>first + second;
+const getFullName = (first,last)=>first + '' +last ;
+const multiply = (a,b) =>a * b;
+const result = multiply(7 ,6);
+console.log(result);
