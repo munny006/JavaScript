2f9a1106c4150602de817521bac343334a30302a
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-02-22 20:28:54 +0600

    disstructing

---
+++ es6/disstructing.js
@@ -0,0 +1,16 @@
+const fish = {
+    name : 'king Hilsa',
+    address : 'chandapur',
+    color:'silver',
+    phone: '0175453223',
+    price : 40000
+}
+const phone = fish.phone;
+console.log(phone);
+
+const {age,name} = {name:'mahi',age:56,professional:'make artist'}
+console.log(age);
+
+//array disstructuring 
+const [first,another] = [44,99]
+console.log(first,another);
