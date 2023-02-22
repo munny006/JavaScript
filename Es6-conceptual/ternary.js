ea21c66c228fa442db6b367d0d99957a5def27cf
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-02-22 21:44:36 +0600

    ternary

---
+++ Es6-conceptual/ternary.js
@@ -0,0 +1,11 @@
+const isString = true;
+let x = 5;
+// if(isString === true){
+//     console.log('he is a student');
+// }
+// else{
+//     console.log('porashuna vala lage na');
+// }
+
+
+ (x === 5) ? console.log('he is student') : console.log('no student');
