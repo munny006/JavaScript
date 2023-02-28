8b8beaa2834555be1a39ab3b578d7fc435336a8b
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-02-28 20:31:25 +0600

    closer

---
+++ common-javascript/scope.js
@@ -0,0 +1,10 @@
+function add(a,b){
+    const total = a + b;
+    if( b > 5 ){
+        const sum = 35 + a +b
+        console.log(sum);
+    }
+    // console.log(total);
+    return total;
+}
+add(5,7)
