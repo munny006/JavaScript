67c7ea832c2b300d42d6d00eaf20aafb204bbd1b
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-02-28 13:25:28 +0600

    common concept

---
+++ common-javascript/type.js
@@ -0,0 +1,21 @@
+// premitives
+const a = 6;
+const b = 'hello mahmuda';
+const d = true;
+
+
+//non premitives
+const ages = [56,78,89];
+const student = {id:006,class:6};
+// console.log(typeof a,typeof b,typeof d,typeof ages,typeof student);
+let x = 5 ;
+let y = x;
+console.log(x,y);
+y = 7;
+console.log(x,y);
+
+let p = {job:'web developer'}
+let q = p;
+console.log(p,q);
+q.job = 'backend developer'
+console.log(p,q);
