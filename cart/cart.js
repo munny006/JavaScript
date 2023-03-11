a9ba7e2d4cfbc90b2e34d5353121ef9d93715d4c
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-03-11 16:09:32 +0600

    displayproduct done

--- cart/cart.js
+++ cart/cart.js
@@ -6,4 +6,11 @@ const addProduct = () => {
     productField.value = '';
     quantityField.value = '';
     console.log(product,quantity);
+    displayProduct(product,quantity);
+}
+const displayProduct = (product,quantity) =>{
+    const ul = document.getElementById('product-container')
+    const li = document.createElement('li')
+    li.innerText = `${product} : ${quantity}`;
+    ul.appendChild(li);
 }
