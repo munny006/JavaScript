a57cee0ef3fa01a9e1246c987a99bacee4b427b1
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-03-13 01:52:31 +0600

    more updated

---
+++ js-concept-react/array.js
@@ -0,0 +1,10 @@
+const product = [
+    {
+        name: 'phone',
+        price: 22000,
+        brand : 'Iphone',
+        color: 'silver',
+    };
+    {name: 'Desktop',price: 232000, brand : 'lenovo', color: 'gold'};
+    {name: 'watch',price: 14000, brand : 'lenovo',color: 'red'};
+]
